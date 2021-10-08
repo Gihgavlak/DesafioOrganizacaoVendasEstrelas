748097ca24776effe999cce19cddd701ba64ab93
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-10-08 15:09:33 -0300

    Estrutura Cliente

--- src/br/com/zup/Cliente.java
+++ src/br/com/zup/Cliente.java
@@ -1,4 +1,13 @@
 package br.com.zup;
 
-public class Cliente {
+public class Cliente extends Pessoa {
+
+    public Cliente(String nome, String cpf, String email) {
+        super(nome, cpf, email);
+    }
+
+    public Cliente() {
+
+    }
+
 }
