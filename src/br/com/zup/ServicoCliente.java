e9d73bd5906c6b4114b36070e56e4d1dade71d65
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-10-08 16:29:44 -0300

    Fazer cadastro do Cliente

--- src/br/com/zup/ServicoCliente.java
+++ src/br/com/zup/ServicoCliente.java
@@ -1,4 +1,16 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicoCliente {
+
+    public static List<Cliente> clientes = new ArrayList<>();
+
+    // Cadastrar cliente ==>
+    public static Cliente cadastrarCliente(String nome, String cpf, String email){
+        Cliente clienteCadastrado = new Cliente (nome, cpf, email);
+        clientes.add(clienteCadastrado);
+        return clienteCadastrado;
+    }
 }
--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,4 +1,21 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
+    private static Scanner obterDados (String mensagem){
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+
+    }
+
+    // <== Cadastrar Cliente
+    public static Cliente cadastrarCliente(){
+        System.out.println("-------------------------------------");
+        String nome = obterDados("Digite o nome do cliente: ").nextLine();
+        String cpf = obterDados("Digite o  CPF do cliente: ").nextLine();
+        String email = obterDados("digite o e-mail do cliente: ").nextLine();
+        return ServicoCliente.cadastrarCliente(nome,cpf,email);
+
+    }
 }
