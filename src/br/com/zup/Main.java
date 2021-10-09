33c6617af11c23ba2ba5a595a614c1f6b1dd7b1f
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-10-08 22:13:38 -0300

    Testando maétodos no main

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -3,11 +3,31 @@ package br.com.zup;
 public class Main {
 
     public static void main(String[] args) {
-	Sistema.cadastrarCliente();
-	ServicoCliente.exibirClientes();
+	//Sistema.cadastrarCliente();
+	//ServicoCliente.exibirClientes();
 
-	Sistema.cadastrarVendendorRes();
-	ServicoVendedorResponsavel.exibirVendedor();
+	//Sistema.cadastrarVendendorRes();
+	//ServicoVendedorResponsavel.exibirVendedor();
+
+		Cliente cliente1 = new Cliente("Gislaine","987","gis@");
+		Cliente cliente2 = new Cliente("Amanda","222","amam@");
+		Cliente cliente3 = new Cliente("Rafaela","444","raf@");
+
+
+		VendedorResponsavel vendedorResponsavel1 = new VendedorResponsavel("Odair","654","odair@");
+		VendedorResponsavel vendedorResponsavel2 = new VendedorResponsavel("Kelly","555","kel@");
+		VendedorResponsavel vendedorResponsavel3 = new VendedorResponsavel("Milly","777","mil@");
+
+		//Venda venda1 = new Venda("Sapato",150,cliente1,vendedorResponsavel1,150,"25/07/2021");
+		//System.out.println(venda1);
+
+		//Venda venda2 = new Venda("Sandalia",80,cliente2,vendedorResponsavel2,80,"10/09/2021");
+		//System.out.println(venda2);
+
+		//Venda venda3 = new Venda("Sapato",150,cliente3,vendedorResponsavel3,90,"19/08/2021");
+		//System.out.println(venda3);
+		ServicoVenda.cadastrarVenda("sapato",155,cliente1,vendedorResponsavel1,150,"25/09/2021");
+		ServicoVenda.exibirVendas();
 
     }
 }
