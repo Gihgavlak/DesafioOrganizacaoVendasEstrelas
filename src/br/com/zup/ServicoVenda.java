17b74713ffb46dccfd207fa41ae9f52febafe4bf
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-10-08 22:11:41 -0300

    Criando metodo de cadastrar e exibir vendas

--- src/br/com/zup/ServicoVenda.java
+++ src/br/com/zup/ServicoVenda.java
@@ -1,4 +1,23 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicoVenda {
+    private static List<Venda> vendas = new ArrayList<>();
+
+    public static Venda cadastrarVenda(String produto,double valorProduto,Cliente cliente,VendedorResponsavel vendedorResponsavel,double valorSerPago, String dataDeVencimento){
+        Venda venda = new Venda(produto, valorProduto, cliente, vendedorResponsavel, valorSerPago, dataDeVencimento);
+        vendas.add(venda);
+        return venda;
+    }
+
+    public static void exibirVendas(){
+        for (Venda referencia:vendas) {
+            System.out.println(referencia);
+
+        }
+    }
+
+
 }
